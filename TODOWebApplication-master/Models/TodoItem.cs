1b29e9a1234ef0360208913cb639e099a3a24282
Marija <82449871+mkasal@users.noreply.github.com>
2022-10-21 00:22:50 +0200

    Add files via upload

---
+++ TODOWebApplication-master/Models/TodoItem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using TODOHelperLibrary;
+using TODOWebApplication.Interfaces;
+
+namespace TODOWebApplication.Models
+{
+    public class TodoItem
+    {
+
+        public int ID { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9\s\.,'""\*]*$", ErrorMessage = "The title contains invalid symbols")]
+        [StringLength(20)]
+        public string Title { get; set; }
+
+        [RegularExpression(@"^[a-zA-Z0-9\s/.,'""\*]*$", ErrorMessage = "The title contains invalid symbols")]
+        [StringLength(200)]
+        public string Description { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Added")]
+        public DateTime AddedDate { get; set; }
+
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Expires")]
+        public DateTime ExpirationDate { get; set; }
+        
+        [Required]
+        [Column(TypeName = "tinyint")]
+        public ItemStatus Status { get; set; }
+    }
+}
---
+++ TODOWebApplication-master/Pages/Index.cshtml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TODOWebApplication.Data;
+using TODOWebApplication.Interfaces;
+using TODOWebApplication.Models;
+
+namespace TODOWebApplication.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly TODOWebApplication.Data.TODOWebApplicationContext _context;
+        public Interfaces.IProgressChecker _progress { get; set; }
+
+        public IndexModel(TODOWebApplication.Data.TODOWebApplicationContext context)
+        {
+            _context = context;
+            _progress = new ProgressChecker(context);
+        }
+
+        // #TODO Fix DI
+
+        /*public IndexModel(TODOWebApplication.Data.TODOWebApplicationContext context)
+            : this(context, new ProgressChecker(context))
+        {
+        }*/
+
+
+        public IList<TodoItem> Tasks { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            var tasks = from x in _context.Tasks
+                        select x;
+
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                tasks = tasks.Where(t => t.Title.Contains(SearchString) ||
+                                        t.Description.Contains(SearchString));
+            }
+
+            Tasks = await tasks.ToListAsync();
+
+            ExpirationChecker expirationChecker = new ExpirationChecker((List<TodoItem>)Tasks);
+            expirationChecker.Expire();
+
+            _ = await _context.SaveChangesAsync();
+
+            _progress.Count();
+
+        }
+    }
+}
